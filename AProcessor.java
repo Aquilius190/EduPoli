40b3d6525c4de0837351c27c7f4742a0bccbe044
Aquilius190 <connorpds@sbcglobal.net>
2015-03-24 12:49:03 -0500

    sweg
    kewl

---
+++ AProcessor.java
@@ -0,0 +1,42 @@
+/**
+ * @(#)AProcessor.java
+ *
+ *
+ * @author
+ * @version 1.00 2015/3/24
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+//import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.net.MalformedURLException;
+//import java.net.URL;
+//import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class AProcessor {
+
+    public AProcessor() {
+		ArrayList<String> r1 = new ArrayList<String>();
+		ArrayList<String> r2 = new ArrayList<String>();
+		ArrayList<String> r3 = new ArrayList<String>();
+		ArrayList<Article> DomEconomics = new ArrayList<Article>();
+		ArrayList<Article> ForeignEconomics = new ArrayList<Article>();
+		ArrayList<Article> ArmedConflict = new ArrayList<Article>();
+		ArrayList<Article> DomesticPolitics = new ArrayList<Article>();
+		ArrayList<Article> ForeignPolitics = new ArrayList<Article>();
+		ArrayList<Article> Elections = new ArrayList<Article>();
+
+
+
+
+
+    }
+
+
+}
---
+++ Driver.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)Driver.java
+ *
+ *
+ * @author
+ * @version 1.00 2015/3/24
+ */
+
+public class Driver {
+
+    public static void main(String[] args)
+    {
+
+    }
+}
